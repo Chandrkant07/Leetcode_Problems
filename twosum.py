5e64209168f17e2395328dc82fb29386ffd6bf92
Chandrkant07 <147088311+Chandrkant07@users.noreply.github.com>
2026-07-18 06:19:25 +0530

    Add: LeetCode 88 - Merge Sorted Array and restore Two Sum

--- twosum.py
+++ twosum.py
@@ -11,10 +11,3 @@ class Solution:
                 return [hashmap[complement], i]
             hashmap[num] = i
         return []
-
-# Example usage
-if __name__ == "__main__":
-    sol = Solution()
-    print(sol.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
-    print(sol.twoSum([3, 2, 4], 6))        # Output: [1, 2]
-    print(sol.twoSum([3, 3], 6))           # Output: [0, 1]
