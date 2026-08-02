522e58343da7a51bac55dad9512b8bdb2094425f
Chandrkant07 <147088311+Chandrkant07@users.noreply.github.com>
2026-08-02 10:06:39 +0530

    Add 877. Stone Game - Optimized O(1) solution

---
+++ StoneGame.py
@@ -0,0 +1,7 @@
+# 877. Stone Game
+# Alice always wins - she can choose all odd or even indexed piles,
+# and since total sum is odd, one group is always larger.
+
+class Solution:
+    def stoneGame(self, piles: list[int]) -> bool:
+        return True
