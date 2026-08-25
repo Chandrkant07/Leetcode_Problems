e0e41d41f8ebb9eafc87028b8a6329229e6942df
Chandrakanta Jena <147088311+Chandrkant07@users.noreply.github.com>
2026-08-25 11:48:13 +0530

    Add solution for LeetCode 3718: Smallest Missing Multiple of K

---
+++ 3718_smallest_missing_multiple_of_k.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        s = set(nums)
+        multiple = k
+        while multiple in s:
+            multiple += k
+        return multiple
