392a07f608b9580db1797effcbeb28c585a8655e
Chandrakanta Jena <147088311+Chandrkant07@users.noreply.github.com>
2026-08-26 12:08:21 +0530

    Add solution for LeetCode 2904: Shortest and Lexicographically Smallest Beautiful String

---
+++ 2904_shortest_and_lexicographically_smallest_beautiful_string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        ones = [i for i, char in enumerate(s) if char == '1']
+
+        if len(ones) < k:
+            return ""
+
+        ans = ""
+        min_len = float('inf')
+
+        for i in range(len(ones) - k + 1):
+            start = ones[i]
+            end = ones[i + k - 1]
+
+            sub = s[start:end + 1]
+
+            if len(sub) < min_len:
+                min_len = len(sub)
+                ans = sub
+            elif len(sub) == min_len and sub < ans:
+                ans = sub
+
+        return ans
